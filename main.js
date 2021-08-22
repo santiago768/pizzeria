950c5a56b28d6e47e8c6194b16b3b9b2874fd78d
santiago768 <83921416+santiago768@users.noreply.github.com>
2021-08-22 15:34:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Margherita Pizza","chese pizza","beef pizza","peperony pizza"
+  ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+         
+}
+
+function add_item(){
+var htmldata;
+var Item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
+function sort(){
+    student_array.sort();
+}
